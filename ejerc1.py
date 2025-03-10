45123447a5430ade8694234e4475a73019e97a57
DavidPineda02 <davxpa.02@gmail.com>
2025-03-10 08:14:22 -0500

    Primer Ejercicio

---
+++ ejerc1.py
@@ -0,0 +1,34 @@
+# Que calcule el gasto de agua en una vivienda dado el número de litros gastados, siendo el sistema de cobro:  
+# • La cuota fija mensual es de 600 pesos 
+# • Los primeros 50 litros son gratis (opción 1)  
+# • Mayor de 50 y 200 litros se cobra el litro a 1000 pesos (opción 2)  
+# • Mayor de 200 litros se cobra el litro a 3000 pesos (opción 3)  
+# • Indicación: hazlo con tres ‘SI’, uno por cada opción. 
+
+def calcularGasto(cantAgua):
+    if cantAgua >= 1 and cantAgua <= 50:
+        return 0
+    elif cantAgua > 50 and cantAgua <= 200:
+        return cantAgua * 1000
+    elif cantAgua > 200:
+        return cantAgua * 3000
+    else:
+        return -1
+
+def mostrarRecibo(cantAgua, valPag):
+    if valPag <= -1:
+        print("\nEl Numero Ingresado es Negativo.")
+        print("\n===========================================")
+    else:
+        print("\n----- Recibo del Agua -----")
+        print(f"Cantidad Gastada: {cantAgua} litros")
+        print(f"Valor a Pagar: ${valPag:,.0f}")
+    print("\n===========================================")
+
+def cobroAgua():
+    print("========== Cobro de Agua Gastada ==========")
+    cantAgua = int(input("\nDigite la Cantidad de Agua Gastada en Litros: "))
+    valPag = calcularGasto(cantAgua)
+    mostrarRecibo(cantAgua, valPag)
+
+cobroAgua()
