fe0110b052979a54ed24668fe7f825e5706938e9
DavidPineda02 <davxpa.02@gmail.com>
2025-03-12 14:15:01 -0500

    Quinto Ejercicio - Numero Factorial

---
+++ ejerc5.py
@@ -0,0 +1,24 @@
+# Algoritmo Calcular la factorial de un número N.
+
+def calcular_factorial(n):
+    if n < 0:
+        return "El Numero Ingresado es Negativo."
+    elif n == 0:
+        return 1
+    else:
+        numeros = []
+        for i in range(1, n + 1):
+            numeros.append(i)
+        
+        factorial = 1
+        while numeros:
+            factorial *= numeros.pop()
+        
+        return factorial
+
+print("========== Numero Factorial ==========")
+num = int(input("\nDigite un Numero para Calcular su Factorial: "))
+resultado = calcular_factorial(num)
+
+print(f"El Factorial de {num} es: {resultado}")
+print("\n======================================")
